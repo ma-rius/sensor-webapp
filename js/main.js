6adbf7f6d605f6d9b0401e5e244c26d4ca1d5e81
Marius Mühlthaler <marius.muehlthaler@student.kit.edu>
2019-08-10 15:11:34 +0200

    first running version

--- js/main.js
+++ js/main.js
@@ -71,6 +71,7 @@ require(["js/influent"], function (influent) {
 document.getElementById('use_app').onchange = function () {
     if (this.checked) {
         show('values_debug');
+
         // Write DeviceOrientation to buffer
         if (window.DeviceOrientationEvent) {
             window.addEventListener('deviceorientation', function (orientation) {
@@ -106,21 +107,111 @@ document.getElementById('use_app').onchange = function () {
         } else {
             document.getElementById("debug").innerHTML = "DeviceMotion not supported."
         }
+        interval_write_to_array = window.setInterval(write_to_array, 1000 / UPLOAD_RATE);
     }
     // interval_orientation = window.setInterval(write_data, 1000 / UPLOAD_RATE);
     // interval_acceleration = window.setInterval(write_acceleration, 1000 / UPLOAD_RATE);
 
     // use one periodically called function for synchronized time stamps in DB
-    // interval = window.setInterval(write_data, 1000 / UPLOAD_RATE);
 
 
     else {
         // window.clearInterval(interval_orientation);
         // window.clearInterval(interval_acceleration);
-        // window.clearInterval(interval);
+        window.clearInterval(interval_write_to_array);
+        rolling_buf = new RollingValues();
+        count_buf = 0;
         // write_data();
         // write_acceleration();
         hide('values_debug');
+        document.body.style.backgroundColor = "";
+    }
+};
+
+function RollingValues() {
+    return {
+        orientation_alpha: [],
+        orientation_beta: [],
+        orientation_gamma: [],
+        acceleration_x: [],
+        acceleration_y: [],
+        acceleration_z: []
+    }
+}
+
+rolling_buf = new RollingValues();
+count_buf = 0;
+write_to_array = function () {
+
+    // console.log(buf);
+    // TODO
+    // if (buf.orientation_alpha.length > 0 && buf.acceleration_x.length > 0) {
+    if (buf.orientation_alpha.length > 0) {
+        // console.log('alpha', buf.orientation_alpha.length);
+        // console.log('beta', buf.orientation_beta.length);
+        // console.log('gamma', buf.orientation_gamma.length);
+        // console.log('x', buf.acceleration_x.length);
+        // console.log('y', buf.acceleration_y.length);
+        // console.log('z', buf.acceleration_z.length);
+
+        var v = buf; //copy old buffer to var to avoid async effects on write_data
+        buf = new Values(); // reset buffer
+
+        function milli_mean_int(values) {
+            var sum = values.reduce(function (a, b) {
+                return a + b;
+            });
+            return Math.floor(sum * 1000 / values.length);
+        }
+
+        // gets written 20 times a second
+        count_buf++;
+        rolling_buf.orientation_alpha.push(milli_mean_int(v.orientation_alpha));
+        rolling_buf.orientation_beta.push(milli_mean_int(v.orientation_beta));
+        rolling_buf.orientation_gamma.push(milli_mean_int(v.orientation_gamma));
+        rolling_buf.acceleration_x.push(milli_mean_int(v.acceleration_x));
+        rolling_buf.acceleration_y.push(milli_mean_int(v.acceleration_y));
+        rolling_buf.acceleration_z.push(milli_mean_int(v.acceleration_z));
+
+
+        if(count_buf > 19){
+            // make POST request to openscoring server to get prediction
+            // console.log(Math.min(...rolling_buf.orientation_alpha));
+            // console.log(...rolling_buf.orientation_alpha);
+
+            $.ajax({
+                url: "https://193.196.37.63/ajax/openscoring/model/SupportVectorMachine",
+                type: "POST",
+                data: JSON.stringify({
+                    "id": "record--01",
+                    "arguments": {
+                        "acceleration_y_rolling_max": Math.max(...rolling_buf.acceleration_y),
+                        "acceleration_y_rolling_min": Math.min(...rolling_buf.acceleration_y)
+                    }
+                }),
+                contentType: 'application/json',
+                dataType: 'json',
+                success: function (result) {
+                    document.getElementById("prediction").innerHTML = Date.now() + ':  '+ JSON.stringify(result);
+                    if(result["results"]["y"] === "running"){
+                        document.body.style.backgroundColor = "red";
+                    } else{
+                        document.body.style.backgroundColor = "green";
+                    }
+
+                }});
+
+
+            // reset
+            rolling_buf = new RollingValues();
+            count_buf = 0;
+
+
+
+        }
+        // console.log(count_buf);
+    } else {
+        // console.log('Either no orientation or no acceleration data - nothing has been written to the DB');
     }
 };
 
